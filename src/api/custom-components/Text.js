31ac5354744664d18c6a342055a6455d1ba70b2f
Piotr Kedra <piotr.k9750@gmail.com>
2021-04-11 21:39:54 +0200

    final

--- src/api/custom-components/Text.js
+++ src/api/custom-components/Text.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-// eslint-disable-next-line react/jsx-props-no-spreading,react/destructuring-assignment
-export default (props) => <Text {...props} style={[{ fontFamily: 'Comfortaa-Regular', fontSize: 17 }, props.style]}>{props.children}</Text>;
+export default (props) => (
+  <Text {...props} style={[{ fontFamily: 'Comfortaa-Regular', fontSize: 17 }, props.style]}>
+    {props.children}
+  </Text>
+);
